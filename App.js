3d37dc5658bc148e82b5a3bc4c93c7e4ef2bb917
LiamOsler <liam.osler@dal.ca>
2022-10-24 18:35:57 -0300

    Basic Application Template

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+import {HTMLContent} from './Webmap.js'; 
+
+const SplashPageContent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Splash Page</Text>
+    </View>
+  );
+}
+
+
+const MapPageContent = () => {
+  console.log("hello world");
+  return (
+    <WebView
+      originWhitelist={['*']}
+      javaScriptEnabledAndroid={true}
+      style={styles.container}
+      source={{ html: HTMLContent}}
+    />
+  );
+}
 
 export default function App() {
+  const [name, setName] = React.useState('Liam');
+  const [content, setContent] = React.useState(
+    (
+      <SplashPageContent />
+    )
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {styles.container}>
+      <StatusBar   
+        backgroundColor = "#b3e6ff"  
+        barStyle = "dark-content"   
+        hidden = {false}    
+        translucent = {true}   
+      />
+      <View style={styles.body}>
+        <MapPageContent/>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  body: {
+    flex: 1,
+    marginTop : StatusBar.currentHeight,
+  }
 });
